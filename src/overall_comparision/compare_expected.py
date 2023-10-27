32fc50d4923c2c444f34c827e41c777e8d80d598
LohmannJens <jens.johann@outlook.de>
2023-10-27 14:44:59 +0200

    Added start vs end sequence length plots

--- src/overall_comparision/compare_expected.py
+++ src/overall_comparision/compare_expected.py
@@ -5,83 +5,15 @@ import os
 import sys
 
 import numpy as np
-#import pandas as pd
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+
+from matplotlib.ticker import FixedLocator, FixedFormatter
 
 sys.path.insert(0, "..")
 from utils import load_all
 from utils import get_sequence, count_direct_repeats_overall, include_correction, get_p_value_symbol, create_nucleotide_ratio_matrix, plot_heatmap
 from utils import SEGMENTS, RESULTSPATH, NUCLEOTIDES
-
-
-def plot_nucleotide_ratio_around_deletion_junction_heatmaps(dfs, dfnames):
-    '''
-        Plot heatmaps of nucleotide ratios around deletion junctions.
-
-    Args:
-        dfs (list of pandas.DataFrame): The list of DataFrames containing the data. 
-                                        Each dataframe should be preprocessed with sequence_df(df)
-        dfnames (list of str): The names associated with each DataFrame in `dfs`.
-        height (float, optional): The height of the figure in inches. Default is 20.
-        width (float, optional): The width of the figure in inches. Default is 16.
-        nucleotides (list of str, optional): The nucleotides to be plotted. Default is ["A", "C", "G", "T"].
-
-    Returns:
-        tuple: A tuple containing the figure and the axes of the subplots.
-            - fig (matplotlib.figure.Figure): The generated figure.
-            - axs (numpy.ndarray of matplotlib.axes.Axes): The axes of the subplots.
-
-    '''
-    fig, axs = plt.subplots(figsize=(13, len(dfs)), nrows=2, ncols=2)
-    axs = axs.flatten()
-
-    for i, nuc in enumerate(NUCLEOTIDES.keys()):
-        x = list()
-        y = list()
-        vals = list()
-        for dfname, df in zip(dfnames, dfs):
-            probability_matrix = create_nucleotide_ratio_matrix(df, "seq_around_deletion_junction")
-            for j in probability_matrix.index:
-                x.append(j)
-                y.append(dfname)
-                vals.append(probability_matrix.loc[j, nuc] * 100)
-                
-        axs[i] = plot_heatmap(x,y,vals, axs[i], vmin=min(vals), vmax=max(vals), cbar=True, format=".0f")
-        
-        for val_label in axs[i].texts:
-            val_label.set_size(8)
-        axs[i].set_title(f"{NUCLEOTIDES[nuc]}")
-        axs[i].set_ylabel("")
-        axs[i].set_yticks([ytick + 0.5 for ytick in range(len(dfnames))])
-        axs[i].set_xlabel("position")  
-        axs[i].set_xticks([xtick - 0.5 for xtick in probability_matrix.index])
-        
-        quarter = len(probability_matrix.index) // 4
-        indexes = [pos for pos in range(1, quarter * 2 + 1)]
-        if i % 2 == 0:
-            axs[i].set_yticklabels([f"{dfname} ({len(df)})" for dfname, df in zip(dfnames, dfs)])
-        else:
-            axs[i].set_yticklabels([])
-
-        if i < 2:
-            axs[i].xaxis.set_ticks_position("top")
-            axs[i].xaxis.set_label_position("top")
-
-        axs[i].set_xticklabels(indexes + indexes)
-        xlabels = axs[i].get_xticklabels()
-        for x_idx, xlabel in enumerate(xlabels):
-            if x_idx < quarter or x_idx >= quarter * 3:
-                xlabel.set_color("black")
-                xlabel.set_fontweight("bold")
-            else:
-                xlabel.set_color("grey") 
-          
-    fig.subplots_adjust(top=0.9)
-
-    save_path = os.path.join(RESULTSPATH, "heatmaps", "nuc_occ.png")
-    plt.savefig(save_path)
-    plt.close()
 
 
 def plot_expected_vs_observed_nucleotide_enrichment_heatmaps(dfs, dfnames, expected_dfs):
@@ -184,70 +116,7 @@ def plot_expected_vs_observed_nucleotide_enrichment_heatmaps(dfs, dfnames, expec
 
     fig.subplots_adjust(top=0.9)
 
-    save_path = os.path.join(RESULTSPATH, "heatmaps", "nuc_occ_diff.png")
-    plt.savefig(save_path)
-    plt.close()
-
-
-def plot_direct_repeat_ratio_heatmaps(dfs: list, dfnames: list)-> None:
-    '''
-        Plot heatmaps of nucleotide ratios around deletion junctions.
-
-    Args:
-        dfs (list of pandas.DataFrame): The list of DataFrames containing the data. 
-                                        Each dataframe should be preprocessed with sequence_df(df)
-        dfnames (list of str): The names associated with each DataFrame in `dfs`.
-        col (str, optional): The column name in the DataFrames that contains the sequence segments of interest. 
-                             Default is "seq_around_deletion_junction".
-        height (float, optional): The height of the figure in inches. Default is 20.
-        width (float, optional): The width of the figure in inches. Default is 16.
-        nucleotides (list of str, optional): The nucleotides to be plotted. Default is ["A", "C", "G", "T"].
-
-    :return: None
-
-    '''
-    fig, axs = plt.subplots(figsize=(10, len(dfs)/2))
-
-    x = list()
-    y = list()
-    vals = list()
-    
-    for dfname, df in zip(dfnames, dfs):
-        final_d = dict()
-
-        for s in SEGMENTS:
-            df_s = df[df["Segment"] == s]
-            if len(df_s) == 0:
-                continue
-            
-            seq = get_sequence(df_s["Strain"].unique()[0], s)
-            
-            counts, _ = count_direct_repeats_overall(df_s, seq)
-            counts = include_correction(counts)
-            
-            for k, v in counts.items():
-                if k in final_d:
-                    final_d[k] += v
-                else:
-                    final_d[k] = v
-
-        x.extend(final_d.keys())
-        y.extend([f"{dfname} ({len(df)})" for _ in range(6)])
-        final = np.array(list(final_d.values()))
-        vals.extend(final/final.sum())
-
-    axs = plot_heatmap(x,y,vals, axs, vmin=0, vmax=1, cbar=True, format=".5f")
-    axs.set_title("direct repeat ratios around deletion junction")
-    axs.set_ylabel("")
-    axs.set_xlabel("direct repeat length")
-
-    x_ticks = axs.get_xticklabels()
-    label = x_ticks[-2].get_text()
-    x_ticks[-1].set_text(f"> {label}")
-    axs.set_xticklabels(x_ticks)
-    fig.tight_layout()
-
-    save_path = os.path.join(RESULTSPATH, "heatmaps", "dir_rep.png")
+    save_path = os.path.join(RESULTSPATH, "compare_expected", "nuc_occ_diff.png")
     plt.savefig(save_path)
     plt.close()
 
@@ -343,19 +212,134 @@ def plot_expected_vs_observed_direct_repeat_heatmaps(dfs: list, dfnames: list, e
     axs.set_xticklabels(x_ticks)
     fig.tight_layout()
 
-    save_path = os.path.join(RESULTSPATH, "heatmaps", "dir_rep_diff.png")
+    save_path = os.path.join(RESULTSPATH, "compare_expected", "dir_rep_diff.png")
     plt.savefig(save_path)
     plt.close()
+
+
+def direct_repeat_composition(dfs: list, dfnames: list, expected_dfs: list):
+    '''
+    
+    '''
+    # calculate direct repeats
+    for dfname, df, expected_df in zip(dfnames, dfs, expected_dfs):
+        final_d = dict()
+        expected_final_d = dict()
+        strain = df["Strain"].unique()[0]
+
+        for s in SEGMENTS:
+            df_s = df[df["Segment"] == s]
+            expected_df_s = expected_df[expected_df["Segment"] == s]
+            n_samples = len(df_s)
+            if n_samples == 0:
+                continue
+
+            seq = get_sequence(strain, s)
+            
+            # TODO: maybe include a correction of the direct repeat sequences ???
+            _ , dr_sequences = count_direct_repeats_overall(df_s, seq)          
+            if "_" in dr_sequences:
+                del dr_sequences["_"]
+            final_d[s] = dr_sequences
+
+            _ , exp_dr_sequences = count_direct_repeats_overall(expected_df_s, seq)
+            if "_" in exp_dr_sequences:
+                del exp_dr_sequences["_"]
+            expected_final_d[s] = exp_dr_sequences
+
+        # make a bar plot with direct repeats lengths 1, 2, ,3 ,4, >4
+        fig, axs = plt.subplots(figsize=(10, 5))
+
+        nuc_counts_d = dict({"A": dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0}),
+                            "C": dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0}),
+                            "G": dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0}),
+                            "U": dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
+                            })
+        sum_counts_d = dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
+        for seq_dict in final_d.values():
+            for k, v in seq_dict.items():
+                if len(k) > 4:
+                    l = 5
+                else:
+                    l = len(k)
+                
+                for n in NUCLEOTIDES.keys():
+                    nuc_counts_d[n][l] += k.count(n) * v
+                sum_counts_d[l] += l * v
+
+        bar_width = 0.7
+        bottom = np.zeros(5)
+
+        for n in NUCLEOTIDES:
+            y = np.array(list(nuc_counts_d[n].values())) / np.array(list(sum_counts_d.values()))
+            axs.bar(nuc_counts_d[n].keys(), y, bar_width, label=n, bottom=bottom)
+            bottom += y
+
+        axs.set_title("nucleotide distribution in direct repeat sequences")
+        axs.set_ylabel("frequency")
+        axs.set_xlabel("direct repeat length")
+        plt.gca().xaxis.set_major_locator(FixedLocator([1, 2, 3, 4, 5]))
+        sum_counts_d[">4"] = sum_counts_d.pop(5)
+        labels = [f"{k} (n={v})" for k, v in sum_counts_d.items()]
+        plt.gca().xaxis.set_major_formatter(FixedFormatter(labels))
+        fig.tight_layout()
+
+        save_path = os.path.join(RESULTSPATH, "compare_expected", f"nucleotides_direct_repeats_{dfname}.png")
+        plt.savefig(save_path)
+        plt.close()
+
+        # make a bar plot to compare orig. data, expected data, composition of full sequence (so I have two references)
+        fig, axs = plt.subplots(figsize=(10, 5))
+
+        comp_nuc_counts_d = dict({"A": dict({"orig": 0, "exp": 0, "full": 0}),
+                                  "C": dict({"orig": 0, "exp": 0, "full": 0}),
+                                  "G": dict({"orig": 0, "exp": 0, "full": 0}),
+                                  "U": dict({"orig": 0, "exp": 0, "full": 0})
+                                  })
+        comp_sum_counts_d = dict({"orig": 0, "exp": 0, "full": 0})
+
+        # observed data
+        for n in NUCLEOTIDES:
+            comp_nuc_counts_d[n]["orig"] = sum(nuc_counts_d[n].values())
+            comp_sum_counts_d["orig"] += comp_nuc_counts_d[n]["orig"]
+
+        # expected data
+        for seq_dict in expected_final_d.values():
+            for k, v in seq_dict.items():
+                for n in NUCLEOTIDES:
+                    comp_nuc_counts_d[n]["exp"] += k.count(n) * v
+        for n in NUCLEOTIDES:
+            comp_sum_counts_d["exp"] += comp_nuc_counts_d[n]["exp"]
+
+        # full seq as reference    
+        for s in SEGMENTS:
+            seq = get_sequence(strain, s)
+            for n in NUCLEOTIDES:
+                comp_nuc_counts_d[n]["full"] += seq.count(n)
+        for n in NUCLEOTIDES:
+            comp_sum_counts_d["full"] += comp_nuc_counts_d[n]["full"]
+
+        bar_width = 0.7
+        bottom = np.zeros(3)
+        for n in NUCLEOTIDES:
+            y = np.array(list(comp_nuc_counts_d[n].values())) / np.array(list(comp_sum_counts_d.values()))
+            axs.bar(comp_nuc_counts_d[n].keys(), y, bar_width, label=n, bottom=bottom)
+            bottom += y
+        
+        axs.set_title(f"nucleotide distribution in direct repeat sequences (n={comp_sum_counts_d['orig']})")
+        axs.set_ylabel("frequency")
+        axs.set_xlabel("data")
+        fig.tight_layout()
+
+        save_path = os.path.join(RESULTSPATH, "compare_expected", f"nucleotides_direct_repeats_compared_{dfname}.png")
+        plt.savefig(save_path)
+        plt.close()
 
 
 if __name__ == "__main__":
     plt.style.use("seaborn")
     dfs, dfnames, expected_dfs = load_all(expected=True)
 
-    plot_nucleotide_ratio_around_deletion_junction_heatmaps(dfs, dfnames)
     plot_expected_vs_observed_nucleotide_enrichment_heatmaps(dfs, dfnames, expected_dfs)
-
-    plot_direct_repeat_ratio_heatmaps(dfs, dfnames)
     plot_expected_vs_observed_direct_repeat_heatmaps(dfs, dfnames, expected_dfs)
-
-    
+    direct_repeat_composition(dfs, dfnames, expected_dfs)
--- src/overall_comparision/general_analyses.py
+++ src/overall_comparision/general_analyses.py
@@ -9,11 +9,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+from scipy import stats
 from scipy.stats import chi2_contingency
 
 sys.path.insert(0, "..")
-from utils import load_all, get_seq_len
-from utils import SEGMENTS, RESULTSPATH, DATASET_STRAIN_DICT, CMAP
+from utils import load_all, get_seq_len, get_sequence, plot_heatmap, create_nucleotide_ratio_matrix, count_direct_repeats_overall, include_correction
+from utils import SEGMENTS, RESULTSPATH, DATASET_STRAIN_DICT, CMAP, NUCLEOTIDES
 
 
 def plot_distribution_over_segments(dfs: list, dfnames: list)-> None:
@@ -28,7 +29,7 @@ def plot_distribution_over_segments(dfs: list, dfnames: list)-> None:
 
     :return: None
     '''
-    fig, axs = plt.subplots(figsize=(len(dfs), 6), nrows=1, ncols=1)
+    fig, axs = plt.subplots(figsize=(len(dfs), 5), nrows=1, ncols=1)
     cm = plt.get_cmap(CMAP)
     colors = [cm(1.*i/len(SEGMENTS)) for i in range(len(SEGMENTS))]
 
@@ -52,7 +53,7 @@ def plot_distribution_over_segments(dfs: list, dfnames: list)-> None:
     
     axs.set_ylabel("relative occurrence of segment")
     axs.set_xlabel("dataset")
-    plt.xticks(range(len(dfnames)), dfnames, rotation=45)
+    plt.xticks(range(len(dfnames)), dfnames, rotation=25)
 
     box = axs.get_position()
     axs.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
@@ -74,7 +75,7 @@ def calculate_deletion_shifts(dfs: list, dfnames: list)-> None:
                              Default is "seq_around_deletion_junction".
     :return: None
     '''
-    fig, axs = plt.subplots(figsize=(12, 12), nrows=4, ncols=4)
+    fig, axs = plt.subplots(figsize=(12, 7), nrows=3, ncols=5)
     cm = plt.get_cmap(CMAP)
     colors = [cm(1.*i/3) for i in range(3)]
 
@@ -99,10 +100,9 @@ def calculate_deletion_shifts(dfs: list, dfnames: list)-> None:
         axs[i,j].pie(sorted_shifts, labels=labels, autopct="%1.1f%%", colors=colors, textprops={"size": 14})
 
         j += 1
-        if j == 4:
+        if j == 5:
             i += 1
             j = 0
-
 
     table = np.array(li)
  #   statistic, pvalue, dof, expected_freq = chi2_contingency(table)
@@ -244,7 +244,7 @@ def create_start_end_connection_plot(df: pd.DataFrame,
 
     # add boxes for start and end of DI RNA sequence
     ax.add_patch(plt.Rectangle((0, 0), max_val, 80, alpha=0.7, color="black"))
-    ax.annotate("RNA sequence", (max_val / 2, 10), color="white", ha="center")
+    ax.annotate("RNA sequence", (max_val / 2, 10), color="white", ha="center", fontsize=10)
 
     # change some values to improve figure
     ax.set_xlim(0, max_val)
@@ -269,6 +269,88 @@ def start_end_positions(dfs: list, dfnames: list)-> None:
         for s in SEGMENTS:
             copy_df = df[(df["Segment"] == s)].copy()
             create_start_end_connection_plot(copy_df, dfname, strain, s)
+
+
+def start_vs_end_lengths(dfs, dfnames, limit: int=0)-> None:
+    '''
+        Plots the length of the start against the length of the end of the DI
+        RNA sequences as a scatter plot.
+
+    '''
+    for df, dfname in zip(dfs, dfnames):
+        fig, axs = plt.subplots(2, 4, figsize=(12, 6), tight_layout=True)
+        j = 0
+        for i, s in enumerate(SEGMENTS):
+            df_s = df[df["Segment"] == s].copy()
+            if df_s.shape[0] > 1:
+                df_s["End_L"] = df_s["full_seq"].str.len() - df_s["End"]
+                axs[j,i%4].scatter(df_s["Start"], df_s["End_L"], s=1.0)
+                axs[j,i%4].plot([0, 1], [0, 1], transform=axs[j,i%4].transAxes, c="r", linewidth=0.5, linestyle="--")
+
+                if limit == 0:
+                    max_p = max(df_s["Start"].max(), df_s["End_L"].max())
+                else:
+                    max_p = limit
+                    axs[j,i%4].set_xlim(0, max_p)
+                    axs[j,i%4].set_ylim(0, max_p)
+                    df_s = df_s[(df_s["Start"] <= max_p) & (df_s["End_L"] <= max_p)]
+                if df_s.shape[0] > 1:
+                    pearson = stats.pearsonr(df_s["Start"], df_s["End_L"])
+                else:
+                    pearson = ["NA", "NA"]
+
+                axs[j,i%4].set_xticks([0, max_p/2, max_p])
+                axs[j,i%4].set_yticks([0, max_p/2, max_p])
+                axs[j,i%4].set_aspect("equal", "box")
+
+            else:
+                axs[j,i%4].set_visible(False)
+
+            axs[j,i%4].set_title(f"{s} (r={pearson[0]:.2})")
+            axs[j,i%4].set_xlabel("3' end")
+            axs[j,i%4].set_ylabel("5' end")
+
+            if i == 3:
+                j = 1
+
+        if limit == 0:
+            filename = f"{dfname}_length_start_end.png"
+        else:
+            filename = f"{dfname}_length_start_end_{limit}.png"
+
+        save_path = os.path.join(RESULTSPATH, "general_analysis", filename)
+        plt.savefig(save_path)
+        plt.close()
+
+
+def diff_start_end_lengths(dfs, dfnames)-> None:
+    '''
+
+    '''
+    fig, axs = plt.subplots(1, 1, figsize=(10, 7), tight_layout=True)
+    plot_list = list()
+    position_list = np.arange(0, len(dfs))
+    labels = list()
+
+    for df, dfname in zip(dfs, dfnames):
+        df["End_L"] = df["full_seq"].str.len() - df["End"]
+        l = (df["Start"] - df["End_L"]).to_list()
+        thresh = 300
+        l = [x for x in l if x <= thresh]
+        l = [x for x in l if x >= -thresh]
+        plot_list.append(l)
+        labels.append(f"{dfname} (n={df.shape[0]})")
+
+    axs.violinplot(plot_list, position_list, points=1000, showmedians=True)
+    axs.set_xticks(position_list)
+    axs.set_xticklabels(labels, rotation=25)
+    axs.set_xlabel("Dataset")
+    axs.set_ylabel("Start-End sequence lengths")
+    axs.set_title(f"Difference of start to end sequence lengths (threshold={thresh})")
+
+    save_path = os.path.join(RESULTSPATH, "general_analysis", f"diff_start_end_violinplot.png")
+    plt.savefig(save_path)
+    plt.close()
 
 
 def dataset_distributions(dfs: list, dfnames: list)-> None:
@@ -314,15 +396,149 @@ def dataset_distributions(dfs: list, dfnames: list)-> None:
     stats_df.to_csv(save_path, index=False)
 
 
+def plot_nucleotide_ratio_around_deletion_junction_heatmaps(dfs, dfnames):
+    '''
+        Plot heatmaps of nucleotide ratios around deletion junctions.
+
+    Args:
+        dfs (list of pandas.DataFrame): The list of DataFrames containing the data. 
+                                        Each dataframe should be preprocessed with sequence_df(df)
+        dfnames (list of str): The names associated with each DataFrame in `dfs`.
+        height (float, optional): The height of the figure in inches. Default is 20.
+        width (float, optional): The width of the figure in inches. Default is 16.
+        nucleotides (list of str, optional): The nucleotides to be plotted. Default is ["A", "C", "G", "T"].
+
+    Returns:
+        tuple: A tuple containing the figure and the axes of the subplots.
+            - fig (matplotlib.figure.Figure): The generated figure.
+            - axs (numpy.ndarray of matplotlib.axes.Axes): The axes of the subplots.
+
+    '''
+    fig, axs = plt.subplots(figsize=(13, len(dfs)), nrows=2, ncols=2)
+    axs = axs.flatten()
+
+    for i, nuc in enumerate(NUCLEOTIDES.keys()):
+        x = list()
+        y = list()
+        vals = list()
+        for dfname, df in zip(dfnames, dfs):
+            probability_matrix = create_nucleotide_ratio_matrix(df, "seq_around_deletion_junction")
+            for j in probability_matrix.index:
+                x.append(j)
+                y.append(dfname)
+                vals.append(probability_matrix.loc[j, nuc] * 100)
+                
+        axs[i] = plot_heatmap(x,y,vals, axs[i], vmin=min(vals), vmax=max(vals), cbar=True, format=".0f")
+        
+        for val_label in axs[i].texts:
+            val_label.set_size(8)
+        axs[i].set_title(f"{NUCLEOTIDES[nuc]}")
+        axs[i].set_ylabel("")
+        axs[i].set_yticks([ytick + 0.5 for ytick in range(len(dfnames))])
+        axs[i].set_xlabel("position")  
+        axs[i].set_xticks([xtick - 0.5 for xtick in probability_matrix.index])
+        
+        quarter = len(probability_matrix.index) // 4
+        indexes = [pos for pos in range(1, quarter * 2 + 1)]
+        if i % 2 == 0:
+            axs[i].set_yticklabels([f"{dfname} ({len(df)})" for dfname, df in zip(dfnames, dfs)])
+        else:
+            axs[i].set_yticklabels([])
+
+        if i < 2:
+            axs[i].xaxis.set_ticks_position("top")
+            axs[i].xaxis.set_label_position("top")
+
+        axs[i].set_xticklabels(indexes + indexes)
+        xlabels = axs[i].get_xticklabels()
+        for x_idx, xlabel in enumerate(xlabels):
+            if x_idx < quarter or x_idx >= quarter * 3:
+                xlabel.set_color("black")
+                xlabel.set_fontweight("bold")
+            else:
+                xlabel.set_color("grey") 
+          
+    fig.subplots_adjust(top=0.9)
+
+    save_path = os.path.join(RESULTSPATH, "general_analysis", "nuc_occ.png")
+    plt.savefig(save_path)
+    plt.close()
+
+
+def plot_direct_repeat_ratio_heatmaps(dfs: list, dfnames: list)-> None:
+    '''
+        Plot heatmaps of nucleotide ratios around deletion junctions.
+
+    Args:
+        dfs (list of pandas.DataFrame): The list of DataFrames containing the data. 
+                                        Each dataframe should be preprocessed with sequence_df(df)
+        dfnames (list of str): The names associated with each DataFrame in `dfs`.
+        col (str, optional): The column name in the DataFrames that contains the sequence segments of interest. 
+                             Default is "seq_around_deletion_junction".
+        height (float, optional): The height of the figure in inches. Default is 20.
+        width (float, optional): The width of the figure in inches. Default is 16.
+        nucleotides (list of str, optional): The nucleotides to be plotted. Default is ["A", "C", "G", "T"].
+
+    :return: None
+
+    '''
+    fig, axs = plt.subplots(figsize=(10, len(dfs)/2))
+
+    x = list()
+    y = list()
+    vals = list()
+    
+    for dfname, df in zip(dfnames, dfs):
+        final_d = dict()
+
+        for s in SEGMENTS:
+            df_s = df[df["Segment"] == s]
+            if len(df_s) == 0:
+                continue
+            
+            seq = get_sequence(df_s["Strain"].unique()[0], s)
+            
+            counts, _ = count_direct_repeats_overall(df_s, seq)
+            counts = include_correction(counts)
+            
+            for k, v in counts.items():
+                if k in final_d:
+                    final_d[k] += v
+                else:
+                    final_d[k] = v
+
+        x.extend(final_d.keys())
+        y.extend([f"{dfname} ({len(df)})" for _ in range(6)])
+        final = np.array(list(final_d.values()))
+        vals.extend(final/final.sum())
+
+    axs = plot_heatmap(x,y,vals, axs, vmin=0, vmax=1, cbar=True, format=".5f")
+    axs.set_title("direct repeat ratios around deletion junction")
+    axs.set_ylabel("")
+    axs.set_xlabel("direct repeat length")
+
+    x_ticks = axs.get_xticklabels()
+    label = x_ticks[-2].get_text()
+    x_ticks[-1].set_text(f"> {label}")
+    axs.set_xticklabels(x_ticks)
+    fig.tight_layout()
+
+    save_path = os.path.join(RESULTSPATH, "compare_expected", "dir_rep.png")
+    plt.savefig(save_path)
+    plt.close()
+
+
 if __name__ == "__main__":
     plt.style.use("seaborn")
     dfs, dfnames, expected_dfs = load_all()
 
-   
     dataset_distributions(dfs, dfnames)
     plot_distribution_over_segments(dfs, dfnames)
     calculate_deletion_shifts(dfs, dfnames)
     length_distribution_heatmap(dfs, dfnames)
-    
     length_distribution_violinplot(dfs, dfnames)
     start_end_positions(dfs, dfnames)
+    plot_nucleotide_ratio_around_deletion_junction_heatmaps(dfs, dfnames)
+    plot_direct_repeat_ratio_heatmaps(dfs, dfnames)
+    start_vs_end_lengths(dfs, dfnames, limit=600)
+    diff_start_end_lengths(dfs, dfnames)
