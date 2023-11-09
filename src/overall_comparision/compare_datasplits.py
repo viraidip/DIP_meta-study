1f18ba8816a064371c52720c537188405bfb25ab
LohmannJens <jens.johann@outlook.de>
2023-11-09 14:24:04 +0100

    Added 10 percentile split

--- src/overall_comparision/compare_datasplits.py
+++ src/overall_comparision/compare_datasplits.py
@@ -35,6 +35,14 @@ def split_by_reads(dfs, split):
             high_df = df[df['NGS_read_count'] > perc2].copy()
             low_df = df[df['NGS_read_count'] <= perc1].copy()
 
+        elif split == "10perc":
+            perc1 = df["NGS_read_count"].quantile(q=0.10)
+            perc2 = df["NGS_read_count"].quantile(q=0.90)
+
+            high_df = df[df['NGS_read_count'] > perc2].copy()
+            low_df = df[df['NGS_read_count'] <= perc1].copy()
+
+
         high_count_dfs.append(high_df)
         low_count_dfs.append(low_df)
 
@@ -44,15 +52,15 @@ def split_by_reads(dfs, split):
 if __name__ == "__main__":
     plt.style.use("seaborn")
     dfs, dfnames, _ = load_all()
-    split = "33perc"
-    dfs, expected_dfs = split_by_reads(dfs, split)
+    split = "10perc"
+    high_dfs, low_dfs = split_by_reads(dfs, split)
 
     name = f"{split}_ngscount"
-    plot_expected_vs_observed_nucleotide_enrichment_heatmaps(dfs, dfnames, expected_dfs, name)
-    plot_expected_vs_observed_direct_repeat_heatmaps(dfs, dfnames, expected_dfs, "high count-low count", name)
+    plot_expected_vs_observed_nucleotide_enrichment_heatmaps(high_dfs, dfnames, low_dfs, name)
+    plot_expected_vs_observed_direct_repeat_heatmaps(high_dfs, dfnames, low_dfs, "high count-low count", name)
 
-    plot_distribution_over_segments(dfs, dfnames, f"{name}_high")
-    diff_start_end_lengths(dfs, dfnames, f"{name}_high")
+    plot_distribution_over_segments(high_dfs, dfnames, f"{name}_high")
+    diff_start_end_lengths(high_dfs, dfnames, f"{name}_high")
     
-    plot_distribution_over_segments(expected_dfs, dfnames, f"{name}_low")
-    diff_start_end_lengths(expected_dfs, dfnames, f"{name}_low")
+    plot_distribution_over_segments(low_dfs, dfnames, f"{name}_low")
+    diff_start_end_lengths(low_dfs, dfnames, f"{name}_low")
