919f9bb001de11996c270656bbf72dac55d50e61
LohmannJens <jens.johann@outlook.de>
2023-12-01 17:19:43 +0100

    Included comparision of Wang cell types

--- src/overall_comparision/compare_datasplits.py
+++ src/overall_comparision/compare_datasplits.py
@@ -12,8 +12,8 @@ from compare_expected import plot_expected_vs_observed_nucleotide_enrichment_hea
 from general_analyses import plot_distribution_over_segments, diff_start_end_lengths
 
 sys.path.insert(0, "..")
-from utils import load_all
-from utils import DATASET_STRAIN_DICT
+from utils import load_all, load_dataset, preprocess, join_data
+from utils import DATASET_STRAIN_DICT, CUTOFF
 
 
 def split_by_reads(dfs, split):
@@ -55,15 +55,31 @@ if __name__ == "__main__":
     dfnames = DATASET_STRAIN_DICT.keys()
     dfs, _ = load_all(dfnames)
 
+    ### split by NGS count ###
     split = "10perc"
     high_dfs, low_dfs = split_by_reads(dfs, split)
 
     name = f"{split}_ngscount"
-    plot_expected_vs_observed_nucleotide_enrichment_heatmaps(high_dfs, dfnames, low_dfs, name)
-    plot_expected_vs_observed_direct_repeat_heatmaps(high_dfs, dfnames, low_dfs, "high count-low count", name)
+    plot_expected_vs_observed_nucleotide_enrichment_heatmaps(high_dfs, dfnames, low_dfs, "high count-low count", folder=name)
+    plot_expected_vs_observed_direct_repeat_heatmaps(high_dfs, dfnames, low_dfs, "high count-low count", folder=name)
 
-    plot_distribution_over_segments(high_dfs, dfnames, f"{name}_high")
-    diff_start_end_lengths(high_dfs, dfnames, f"{name}_high")
+    plot_distribution_over_segments(high_dfs, dfnames, folder=f"{name}_high")
+    diff_start_end_lengths(high_dfs, dfnames, folder=f"{name}_high")
     
-    plot_distribution_over_segments(low_dfs, dfnames, f"{name}_low")
-    diff_start_end_lengths(low_dfs, dfnames, f"{name}_low")
+    plot_distribution_over_segments(low_dfs, dfnames, folder=f"{name}_low")
+    diff_start_end_lengths(low_dfs, dfnames, folder=f"{name}_low")
+
+    ### compare two Wang2020 cell types ###
+    df_dict = dict({})
+    dfname = "Wang2020"
+    cell_dfnames = list([dfname])
+    df = load_dataset(dfname)
+
+    for cell_type in ["A549", "HBEpC"]:
+        c_df = df[df["Cell"] == cell_type].copy()
+        
+        df_dict[cell_type] = [preprocess(DATASET_STRAIN_DICT[dfname], join_data(c_df), CUTOFF)]
+    
+    folder = "Wang_cells_compared"
+    plot_expected_vs_observed_nucleotide_enrichment_heatmaps(df_dict["A549"], cell_dfnames, df_dict["HBEpC"], "A549 - HBEpC", folder=folder)
+    plot_expected_vs_observed_direct_repeat_heatmaps(df_dict["A549"], cell_dfnames, df_dict["HBEpC"], "A549 - HBEpC", folder=folder)
