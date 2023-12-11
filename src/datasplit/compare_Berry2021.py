f7d545858b1c7f7953cdeb967765386c01741245
LohmannJens <jens.johann@outlook.de>
2023-12-11 16:33:44 +0100

    Comparision of 3-5 ends for Berry2021 datasets

---
+++ src/datasplit/compare_Berry2021.py
@@ -0,0 +1,59 @@
+'''
+
+'''
+import os
+import sys
+
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+
+sys.path.insert(0, "..")
+from utils import load_all, get_p_value_symbol
+from utils import SEGMENTS
+from overall_comparision.general_analyses import calc_start_end_lengths
+
+
+def compare_3_5_ends(dfs, dfnames):
+    '''
+    
+    '''
+    data = list()
+    labels = list()
+    
+    data, labels = calc_start_end_lengths(dfs, dfnames)
+        
+
+    fig, axs = plt.subplots(1, 1, figsize=(5, 5), tight_layout=True)
+    position_list = np.arange(0, 3)
+    axs.violinplot(data, position_list, points=1000, showmedians=True)
+    axs.set_xticks(position_list)
+    axs.set_xticklabels(labels, rotation=90)
+    axs.set_ylim(top=400)
+    axs.set_xlabel("Dataset")
+    axs.set_ylabel("Start-End sequence lengths")
+    axs.set_title(f"Difference of start to end sequence lengths")
+    
+    def add_significance(l1, l2, axs, start, end, height):
+        _, pvalue = stats.f_oneway(l1, l2)
+        symbol = get_p_value_symbol(pvalue)
+        if symbol != "":
+            axs.plot([start, end], [height, height], lw=2, color='black')
+            axs.text((start + end) / 2, height + 0.01, symbol, ha='center', va='bottom', color='black')
+
+    add_significance(data[0], data[1], axs, 0, 1, 310)
+    add_significance(data[0], data[2], axs, 0, 2, 340)
+    add_significance(data[1], data[2], axs, 1, 2, 370)
+
+    plt.show()
+    plt.close()
+
+
+if __name__ == "__main__":
+    plt.style.use("seaborn")
+
+    dfnames = ["Berry2021_A", "Berry2021_B", "Berry2021_B_yamagata"]
+    dfs, _ = load_all(dfnames)
+
+    compare_3_5_ends(dfs, dfnames)
--- src/datasplit/compare_vivo_vitro.py
+++ src/datasplit/compare_vivo_vitro.py
@@ -16,75 +16,48 @@ from utils import SEGMENTS
 from overall_comparision.general_analyses import calc_DI_lengths, calc_start_end_lengths
 
 
-def compare_DI_lengths(a_dfs, a_dfnames, a_label, b_dfs, b_dfnames, b_label):
+def compare_DI_lengths(a_dfs, a_dfnames, a_label, b_dfs, b_dfnames, b_label, c_dfs, c_dfnames, c_label):
     '''
     
     '''
-    a_lengths_dict = calc_DI_lengths(a_dfs, a_dfnames)
-    a_dict = dict({"PB2": Counter(), "PB1": Counter(), "PA": Counter(), "HA": Counter(), "NP": Counter(), "NA": Counter(), "M": Counter(), "NS": Counter()})
-    for d in a_lengths_dict.values():
-        for s in a_dict.keys():
-            a_dict[s] += Counter(d[s])
-
-    b_lengths_dict = calc_DI_lengths(b_dfs, b_dfnames)   
-    b_dict = dict({"PB2": Counter(), "PB1": Counter(), "PA": Counter(), "HA": Counter(), "NP": Counter(), "NA": Counter(), "M": Counter(), "NS": Counter()})
-    for d in b_lengths_dict.values():
-        for s in b_dict.keys():
-            b_dict[s] += Counter(d[s])
-
+    def process_data(dfs, dfnames):
+        lengths_dict = calc_DI_lengths(dfs, dfnames)
+        final_d = dict({"PB2": Counter(), "PB1": Counter(), "PA": Counter(), "HA": Counter(), "NP": Counter(), "NA": Counter(), "M": Counter(), "NS": Counter()})
+        for d in lengths_dict.values():
+            for s in d.keys():
+                final_d[s] += Counter(d[s])
+        return final_d
+    
+    a_dict = process_data(a_dfs, a_dfnames)
+    b_dict = process_data(b_dfs, b_dfnames)
+    c_dict = process_data(c_dfs, c_dfnames)
+    
+    bins = 30
     for s in SEGMENTS:
         x_a = [key for key, value in a_dict[s].items() for _ in range(value)]
         x_b = [key for key, value in b_dict[s].items() for _ in range(value)]
+        x_c = [key for key, value in c_dict[s].items() for _ in range(value)]
 
-        _, pvalue = stats.f_oneway(x_a, x_b)
-        print(pvalue)
-        symbol = get_p_value_symbol(pvalue)
+        if len(x_a) < 50 or len(x_b) < 50 or len(x_c) < 50:
+            continue
 
-        plt.hist(x_a, alpha=0.5, label="in vitro", bins=20, density=True)
-        plt.hist(x_b, alpha=0.5, label="in vivo", bins=20, density=True)
+        def calc_anova(x_1, x_2):
+            data1, _ = np.histogram(x_1, bins=bins)
+            data2, _ = np.histogram(x_2, bins=bins)
+            _, pvalue = stats.f_oneway(data1, data2)
+            return get_p_value_symbol(pvalue)
+        
+        s_ab = calc_anova(x_a, x_b)        
+        s_ac = calc_anova(x_a, x_c)        
+        s_bc = calc_anova(x_b, x_c)
+
+        plt.hist(x_a, alpha=0.5, label=a_label, bins=bins, density=True)
+        plt.hist(x_b, alpha=0.5, label=b_label, bins=bins, density=True)
+        plt.hist(x_c, alpha=0.5, label=c_label, bins=bins, density=True)
         plt.legend()
-        plt.title(f"{s} {symbol}")
+        plt.title(f"{s} (1-2: {s_ab} | 1-3: {s_ac} | 2-3: {s_bc})")
 
         plt.show()
-        
-
-def compare_3_5_ends(a_dfs, a_dfnames, b_dfs, b_dfnames, labels):
-    '''
-    
-    '''
-    a_list, a_labels = calc_start_end_lengths(a_dfs, a_dfnames)
-    b_list, b_labels = calc_start_end_lengths(b_dfs, b_dfnames)
-
-    fig, axs = plt.subplots(1, 1, figsize=(10, 7), tight_layout=True)
-    position_list = np.arange(0, len(a_dfs) + len(b_dfs))
-    axs.violinplot(a_list+b_list, position_list, points=1000, showmedians=True)
-    axs.set_xticks(position_list)
-    axs.set_xticklabels(a_labels+b_labels, rotation=90)
-    axs.set_xlabel("Dataset")
-    axs.set_ylabel("Start-End sequence lengths")
-    axs.set_title(f"Difference of start to end sequence lengths")
-
-    plt.show()
-    plt.close()
-
-    a_full_list = [item for sublist in a_list for item in sublist]
-    b_full_list = [item for sublist in b_list for item in sublist]
-
-    _, pvalue = stats.f_oneway(a_full_list, b_full_list)
-    print(pvalue)
-    symbol = get_p_value_symbol(pvalue)
-
-    fig, axs = plt.subplots(1, 1, figsize=(10, 7), tight_layout=True)
-    position_list = np.arange(0, 2)
-    axs.violinplot([a_full_list, b_full_list], position_list, points=1000, showmedians=True)
-    axs.set_xticks(position_list)
-    axs.set_xticklabels(labels, rotation=90)
-    axs.set_xlabel("Dataset")
-    axs.set_ylabel("Start-End sequence lengths")
-    axs.set_title(f"Difference of start to end sequence lengths ({symbol})")
-    
-    plt.show()
-    plt.close()
 
 
 if __name__ == "__main__":
@@ -99,5 +72,5 @@ if __name__ == "__main__":
     patient_dfnames = get_dataset_names(cutoff=0, selection="in vivo human")
     patient_dfs, _ = load_all(patient_dfnames)
 
-    compare_DI_lengths(vitro_dfs, vitro_dfnames, "in vitro", vivo_dfs, vivo_dfnames, "in vivo human")
-    compare_3_5_ends(vitro_dfs, vitro_dfnames, patient_dfs, patient_dfnames, ["in vitro", "in vivo humans"])
+    compare_DI_lengths(vitro_dfs, vitro_dfnames, "in vitro", vivo_dfs, vivo_dfnames, "in vivo human", patient_dfs, patient_dfnames, "in vivo humans")
+
